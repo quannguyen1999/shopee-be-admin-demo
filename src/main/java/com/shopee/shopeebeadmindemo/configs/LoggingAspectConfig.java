72c6995dd02668df56a1aad3c57cba72faafe28a
quannguyen1999 <nguyendanganhquan99@gmail.com>
2023-12-25 15:22:01 +0700

    Set Aspect only run on env dev

--- src/main/java/com/shopee/shopeebeadmindemo/configs/LoggingAspectConfig.java
+++ src/main/java/com/shopee/shopeebeadmindemo/configs/LoggingAspectConfig.java
@@ -6,11 +6,13 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 @Slf4j
+@Profile("dev")
 public class LoggingAspectConfig {
     @Pointcut("@annotation(LogNameMethod)")
     public void logPointcut() {
