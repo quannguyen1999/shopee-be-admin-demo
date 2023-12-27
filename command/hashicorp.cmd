ba8d96ff9ba5f810c5f58b8d4bc2fada2712137b
quannguyen1999 <nguyendanganhquan99@gmail.com>
2023-12-27 23:32:35 +0700

    Save log file

--- command/hashicorp.cmd
+++ command/hashicorp.cmd
@@ -1,5 +1,9 @@
-Create: vault kv put
-    secret/shopee_local/prod
+Create Vault:
+    vault server -dev --dev-root-token-id="18456350-7b89-11ed-85e9-a5830358e351";
+
+Create Key/Value:
+    vault kv put
+        secret/shopee_local/prod
         spring.datasource.username=postgres
         spring.datasource.password=postgres;
 
