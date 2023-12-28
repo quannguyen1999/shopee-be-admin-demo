23f5a31d155ce627dc555d6e65e0427ab18b8edf
quannguyen1999 <nguyendanganhquan99@gmail.com>
2023-12-28 11:15:40 +0700

    Fix hashicorp

--- command/hashicorp.cmd
+++ command/hashicorp.cmd
@@ -2,8 +2,8 @@ Create Vault:
     vault server -dev --dev-root-token-id="18456350-7b89-11ed-85e9-a5830358e351";
 
 Create Key/Value:
-    vault kv put
-        secret/shopee_local/prod
-        spring.datasource.username=postgres
-        spring.datasource.password=postgres;
+    vault kv put secret/shopee_local/dev  spring.datasource.username=postgres spring.datasource.password=postgres;
 
+Add Path:
+    set VAULT_ADDR=http://127.0.0.1:8200
+    export VAULT_TOKEN="18456350-7b89-11ed-85e9-a5830358e351"
