7efcf3b1c1e7078adbea80003fe4082c2c441ccd
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-02-09 12:46:00 +0700

    fix bug

--- src/main/resources/db/migration/V20240102121700__addNewColumn.sql
+++ src/main/resources/db/migration/V20240102121700__addNewColumn.sql
@@ -1,10 +1,10 @@
-ALTER TABLE qa_shopee.account
+ALTER TABLE account
     ADD COLUMN created date;
-ALTER TABLE qa_shopee.account
+ALTER TABLE account
     ADD COLUMN updated date;
-ALTER TABLE qa_shopee.account
+ALTER TABLE account
     ADD COLUMN user_created varchar(200);
-ALTER TABLE qa_shopee.account
+ALTER TABLE account
     ADD COLUMN user_updated varchar(200);
-ALTER TABLE qa_shopee.account
+ALTER TABLE account
     ADD COLUMN is_active boolean;
--- src/main/resources/db/migration/V20240105161000__setDefaultTimestamp.sql
+++ src/main/resources/db/migration/V20240105161000__setDefaultTimestamp.sql
@@ -1,5 +1,5 @@
-ALTER TABLE qa_shopee.account
+ALTER TABLE account
     ALTER COLUMN created set DEFAULT current_timestamp::timestamp;
 
-ALTER TABLE qa_shopee.account
+ALTER TABLE account
     ALTER COLUMN updated set DEFAULT current_timestamp::timestamp;
