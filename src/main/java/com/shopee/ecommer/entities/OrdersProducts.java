c4ebf2df0e443437482325acb071e26a3fcc79cf
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-03-29 10:22:43 +0700

    Fix Bug

--- src/main/java/com/shopee/ecommer/entities/OrdersProducts.java
+++ src/main/java/com/shopee/ecommer/entities/OrdersProducts.java
@@ -2,10 +2,13 @@ package com.shopee.ecommer.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 import java.util.UUID;
 
+
+@EqualsAndHashCode
 @Embeddable
 public class OrdersProducts implements Serializable {
     @Column(name = "OrderID")
