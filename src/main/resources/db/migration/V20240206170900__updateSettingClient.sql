4de8d0598910e2c2b28537204448c8e3b0447ffe
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-04-02 09:10:21 +0700

    FIx Bug

--- src/main/resources/db/migration/V20240206170900__updateSettingClient.sql
+++
@@ -1,3 +0,0 @@
-update client
-set redirect_uris = 'http://127.0.0.1:4200/,https://oauthdebugger.com/debug,http://127.0.0.1:4200/home,http://127.0.0.1:8080/login/oauth2/code/api-client-oidc,http://127.0.0.1:8080,http://localhost:4200/home'
-where client_id = 'admin'
--- src/main/resources/db/migration/V20240206220900__updateSettingClient.sql
+++
@@ -1,4 +0,0 @@
-
-update client
-set client_authentication_methods  = 'client_secret_basic,client_secret_post'
-where client_id  = 'admin'
