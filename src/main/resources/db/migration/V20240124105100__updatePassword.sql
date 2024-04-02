5a7c9457a2d156edbcc8d7e569840995857129e0
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-04-02 09:03:05 +0700

    Fix bug flyway

--- src/main/resources/db/migration/V20240124105100__updatePassword.sql
+++ src/main/resources/db/migration/V20240124105100__updatePassword.sql
@@ -1,24 +1,3 @@
 ALTER TABLE account
 ALTER
 COLUMN password TYPE varchar(1000);
-
-update account
-set password = '$2a$10$H2fnzkVd05EUybKAAaI.lO0r.SYHCh1H9JVpFhiJ9Yv/Ne/QM8Diy'
-where username in ('admin', 'employee_sam', 'client_a', 'client_b');
-
-update client
-set scopes = 'read,write'
-where client_name = 'admin';
-
-update client
-set redirect_uris = 'http://127.0.0.1:4200/'
-where client_name = 'admin';
-
-update client
-set client_secret = '$2a$10$H2fnzkVd05EUybKAAaI.lO0r.SYHCh1H9JVpFhiJ9Yv/Ne/QM8Diy'
-where client_name = 'admin';
-
-update client
-set authorization_grant_types = 'authorization_code'
-where client_name = 'admin';
-
--- src/main/resources/db/migration/V20240130170700__updateTableClient.sql
+++
@@ -1,3 +0,0 @@
-update client
-set redirect_uris = 'http://127.0.0.1:4200/,http://127.0.0.1:8080/login/oauth2/code/api-client-oidc,http://127.0.0.1:8080/authorized,http://127.0.0.1:8080,http://127.0.0.1:4200/home'
-where client_id = 'admin';
